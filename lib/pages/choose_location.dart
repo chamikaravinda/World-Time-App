92516babf02a9756c5781128112aeaedca7a1203
Chamika Ravinda <chamikaravinda@gmail.com>
2020-03-19 16:41:48 +0530

    fetching from api and flutter packages

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -7,26 +7,6 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  void getData() async {
-
-    //get name
-    await Future.delayed(Duration(seconds: 3),(){
-      print('yoshi');
-    });
-
-    //get bio
-    Future.delayed(Duration(seconds: 2),(){
-      print('vegan,musician & egg collector');
-    });
-
-    print('Statment');
-  }
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
-
   @override
   Widget build(BuildContext context){
     return Scaffold(
